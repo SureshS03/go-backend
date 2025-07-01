e9af38f85d1a397f3ba4791534daeeb522780a18
SureshS03 <suthamani51@gmail.com>
2025-07-01 22:25:39 +0530

    coded basic redis client and cache (NOT TESTED)

---
+++ internal/redis/cache.go
@@ -0,0 +1,15 @@
+package redis
+
+import "time"
+
+func SetCache(key string, value string, expiration time.Duration) error {
+	return Client.Set(Ctx, key, value, expiration).Err()
+}
+
+func GetCache(key string) (string, error) {
+	return Client.Get(Ctx, key).Result()
+}
+
+func DeleteCache(key string) error {
+	return Client.Del(Ctx, key).Err()
+}
---
+++ internal/redis/client.go
@@ -0,0 +1,18 @@
+package redis
+
+
+import (
+	"context"
+	"github.com/redis/go-redis/v9"
+)
+
+var Client *redis.Client
+var Ctx = context.Background()
+
+func Init() {
+	Client = redis.NewClient(&redis.Options{
+		Addr: "localhost:6379",
+		Password: "",
+		DB: 0,
+	})
+}
