7cffa4dc6d7afd951980b5da4e152b42d2ee41f0
SureshS03 <suthamani51@gmail.com>
2025-07-05 00:12:01 +0530

    added inc func for cache

--- cachehandler.go
+++ cachehandler.go
@@ -9,14 +9,14 @@ import (
 	"github.com/SureshS03/goconnect/internal/redis"
 )
 
-func SetUserCache(user User) error {
+func SetUserCache(user User, min time.Duration) error {
 	data, err := json.Marshal(user)
 	if err != nil {
 		fmt.Println("err in set cache", err)
 		return err
 	}
 	id := strconv.Itoa(user.ID)
-	return redis.SetCache("user:"+id, string(data), time.Minute*8)
+	return redis.SetCache("user:"+id, string(data), time.Minute*min)
 }
 
 func GetUserCache(id string) (*User, error) {
@@ -36,13 +36,13 @@ func GetUserCache(id string) (*User, error) {
 	return &user, nil
 }
 
-func SetPostCache(post Post) error {
+func SetPostCache(post Post, min time.Duration) error {
 	data, err := json.Marshal(post)
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
-	return redis.SetCache("post:"+post.ID, string(data), time.Minute*8)
+	return redis.SetCache("post:"+post.ID, string(data), time.Minute*min)
 }
 
 func GetPostCache(id string) (*Post, error) {
@@ -60,4 +60,22 @@ func GetPostCache(id string) (*Post, error) {
 	}
 
 	return &post, nil
+}
+
+func IncPostsInUser(id string) error {
+	if !redis.CacheExists("user:" + id) {
+		return nil
+	}
+	user, err := GetUserCache(id)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	user.NoOfPost++
+	err = SetUserCache(*user, time.Minute*5)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
 }
