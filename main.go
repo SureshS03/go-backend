56f221ece69815ecf446d2ba128aa79e34c14b5c
SureshS03 <suthamani51@gmail.com>
2025-07-07 22:17:15 +0530

    added migration and correctioms

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 func main() {
 	//s := fmt.Sprintf("user=%v dbname=%v password=%v sslmode=%v", os.Getenv("DB_USER"), os.Getenv("DB_NAME"), os.Getenv("PASSWORD"), os.Getenv("SSL_MODE"))
 	//fmt.Println(s)
-	db := NewDB("postgres", "host=db user=postgres dbname=goconnect password=arya sslmode=disable")
+	db := NewDB("postgres", "host=postgres user=postgres dbname=goconnect password=arya sslmode=disable")
 	if db == nil {
 		fmt.Println("DB connection failed. Exiting...")
 		return
